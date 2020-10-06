a07462b5bc262667f931a39f0bdb9be304b5b727
Cameron Brill <c00brill@gmail.com>
2020-10-05 21:12:53 -0400

    Initial commit.

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", MapHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
---
+++ shortener/handler.go
@@ -0,0 +1,29 @@
+package shortener
+
+import (
+	"net/http"
+)
+
+type pathToURL struct {
+	Path string
+	URL  string
+}
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		url := pathsToUrls[req.URL.Path]
+		if url != "" {
+			http.Redirect(res, req, url, http.StatusPermanentRedirect)
+		} else {
+			fallback.ServeHTTP(res, req)
+		}
+	})
+}
+
+func buildMap(pathsToURLs []pathToURL) (builtMap map[string]string) {
+	builtMap = make(map[string]string)
+	for _, ptu := range pathsToURLs {
+		builtMap[ptu.Path] = ptu.URL
+	}
+	return
+}
