61df4c59d9ac4f57be42a06412b31ff3ff66f8f2
Cameron Brill <c00brill@gmail.com>
2021-03-07 14:18:44 -0700

    CRUD works now.

--- go.mod
+++ go.mod
@@ -4,10 +4,15 @@ go 1.15
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/cosmtrek/air v1.21.2 // indirect
+	github.com/creack/pty v1.1.11 // indirect
+	github.com/fatih/color v1.10.0 // indirect
 	github.com/fzipp/gocyclo v0.3.1 // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/lib/pq v1.9.0
 	github.com/magefile/mage v1.11.0 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
 	github.com/sirupsen/logrus v1.8.0
 	golang.org/x/sys v0.0.0-20210305230114-8fe3ee5dd75b // indirect
 )
--- main.go
+++ main.go
@@ -31,15 +31,16 @@ func getEnv(key, fallback string) string {
 }
 
 func initEnvVars() {
+	var err error
 	DB_HOST = getEnv("DB_HOST", "localhost")
 	DB_USER = getEnv("DB_USER", "postgres")
 	DB_PASSWORD = getEnv("DB_PASSWORD", "postgres")
 	DB_NAME = getEnv("DB_NAME", "postgres")
-	DB_PORT, err := strconv.Atoi(getEnv("DB_PORT", "5432"))
+	DB_PORT, err = strconv.Atoi(getEnv("DB_PORT", "5432"))
 	if err != nil {
 		log.Fatalf("port %v cannot be parsed\n", DB_PORT)
 	}
-	API_PORT, err := strconv.Atoi(getEnv("API_PORT", "8080"))
+	API_PORT, err = strconv.Atoi(getEnv("API_PORT", "8080"))
 	if err != nil {
 		log.Fatalf("port %v cannot be parsed\n", API_PORT)
 	}
@@ -63,13 +64,13 @@ func setupDB() (DB *sql.DB) {
 }
 
 type ShortURLRequest struct {
-	OriginalURL string `json:"original_url"`
-	TinyURL     string `json:"tiny_url,omitempty"`
+	URL      string `json:"url"`
+	ShortURL string `json:"short_url,omitempty"`
 }
 
 type ShortURLResponse struct {
-	NormalizedOriginalURL string `json:"normalized_original_url"`
-	TinyURL               string `json:"tiny_url"`
+	URL      string `json:"url"`
+	ShortURL string `json:"short_url"`
 }
 
 /*
@@ -87,62 +88,108 @@ func createShortLink(w http.ResponseWriter, r *http.Request) {
 	var shortURLReq ShortURLRequest
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&shortURLReq); err != nil {
-		log.Fatalf("invalid request: %+v\nerr:%v\n", r, err)
+		log.Fatalf("invalid request: %+v\nerr:%+v\n", r, err)
 		return
 	}
+	log.Infof("body: %+v", r.Body)
+	log.Infof("creating short_url: {url: %v}{short_url: %v}", shortURLReq.URL, shortURLReq.ShortURL)
 	defer r.Body.Close()
 
 	var id string
 
 	err := DB.QueryRow(
-		"INSERT INTO links(url, shorturl) VALUES(?, ?) RETURNING id",
-		shortURLReq.OriginalURL, shortURLReq.TinyURL).Scan(&id)
+		"INSERT INTO links(url, short_url) VALUES($1, $2) RETURNING id",
+		shortURLReq.URL, shortURLReq.ShortURL).Scan(&id)
 
 	if err != nil {
 		log.Fatalf("failed to upload link to database: %v\n", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func getURLGivenShortURL(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	shortLink := vars["short_url"]
-	var shortURL ShortURLResponse
-	err := DB.QueryRow(
-		"UPDATE links SET unique_visits = unique_visits + 1 WHERE short_url = ? RETURNING (url, short_url);",
-		shortLink).Scan(&shortURL)
 
+	log.Infof("getting url given short_url: {short_url: %s}", shortLink)
+	var shortURL ShortURLResponse
+
+	// query db
+	err := DB.QueryRow(
+		"UPDATE links SET unique_visits = unique_visits + 1 WHERE short_url = $1 RETURNING url, short_url;",
+		shortLink).Scan(&shortURL.URL, &shortURL.ShortURL)
 	if err != nil {
 		log.Fatalf("failed to get url given {short_url: %s} from database: %v\n", shortLink, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	// build response
+	log.Infof("marshalling shortURL into json: %+v", &shortURL)
+	response, err := json.Marshal(&shortURL)
+	if err != nil {
+		log.Fatalf("failed to marshal database response into json: shortURL: %+v\nresponse:%+v\nerr:%v", shortURL, response, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	status, err := w.Write(response)
+	if err != nil {
+		http.Error(w, err.Error(), status)
 	}
 }
 
 func getShortURLGivenURL(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	url := vars["url"]
-	var shortURL ShortURLResponse
-	err := DB.QueryRow(
-		"UPDATE links SET unique_visits = unique_visits + 1 WHERE url = ? RETURNING (url, short_url);",
-		url).Scan(&shortURL)
+	url := r.URL.Query().Get("url")
 
+	log.Infof("getting short_url given url: {url: %s}", url)
+	var shortURL ShortURLResponse
+
+	err := DB.QueryRow(
+		"UPDATE links SET unique_visits = unique_visits + 1 WHERE url = $1 RETURNING url, short_url;",
+		url).Scan(&shortURL.URL, &shortURL.ShortURL)
 	if err != nil {
 		log.Fatalf("failed to get short_url given {url: %s} from database: %v\n", url, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
+	// build response
+	log.Infof("marshalling shortURL into json: %+v", &shortURL)
+	response, err := json.Marshal(&shortURL)
+	if err != nil {
+		log.Fatalf("failed to marshal database response into json: shortURL: %+v\nresponse:%+v\nerr:%v", shortURL, response, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	status, err := w.Write(response)
+	if err != nil {
+		http.Error(w, err.Error(), status)
+	}
+
 }
 
 func main() {
 	// Initialize environment variables
+	log.Infoln("initializing environment variables...")
 	initEnvVars()
 
 	// Setup database
+	log.Infoln("setting up database...")
 	DB = setupDB()
 
 	// Setup routing
+	log.Infoln("setting up routes...")
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/short_url", createShortLink).Methods("POST")
+	router.HandleFunc("/get_short_url", getShortURLGivenURL).Methods("GET").Queries("url", "{url}")
 	router.HandleFunc("/{short_url}", getURLGivenShortURL).Methods("GET")
-	router.HandleFunc("/get_short_url/{url}", getShortURLGivenURL).Methods("GET")
+
+	http.Handle("/", router)
 
 	// Listen on port for API calls
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", API_PORT), nil))
+	log.Infof("starting api server on port:%d...", API_PORT)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", API_PORT), router))
 }
