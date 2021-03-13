579195b30791e40730489355107face6e7a5dbbd
Cameron Brill <c00brill@gmail.com>
2021-03-13 09:38:31 -0700

    Remove heroku-specific stuff, keep it general.

--- app.go
+++ app.go
@@ -25,7 +25,6 @@ var (
 	DB_USER        string
 	DB_PASSWORD    string
 	DB_NAME        string
-	DATABASE_URL   string
 	REDIS_PORT     int
 	REDIS_HOST     string
 	REDIS_PASSWORD string
@@ -50,7 +49,6 @@ func (a *App) initEnvVars() {
 	if err != nil {
 		log.Fatalf("port %v cannot be parsed\n", DB_PORT)
 	}
-	DATABASE_URL = getEnv("DATABASE_URL", "")
 	REDIS_HOST = getEnv("REDIS_HOST", "localhost")
 	REDIS_PASSWORD = getEnv("REDIS_PASSWORD", "")
 	REDIS_PORT, err = strconv.Atoi(getEnv("REDIS_PORT", "6379"))
@@ -67,17 +65,10 @@ func (a *App) setupDB() {
 	log.Infof("setting up database")
 	var err error
 	// Connect to postgres
-	if DATABASE_URL != "" {
-		a.DB, err = sql.Open("postgres", DATABASE_URL)
-		if err != nil {
-			log.Fatalf("connection to database failed: %v\n", err)
-		}
-	} else {
-		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME)
-		a.DB, err = sql.Open("postgres", psqlInfo)
-		if err != nil {
-			log.Fatalf("connection to database failed: %v\n", err)
-		}
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME)
+	a.DB, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("connection to database failed: %v\n", err)
 	}
 
 	// Testing postgres connection
