8d3b5d5ea7ba4eacf4cb7d9bc17163b8ad0519c0
Cameron Brill <c00brill@gmail.com>
2022-05-21 21:11:43 -0700

    fix: meant to rm this before

--- brill.wtf.rs/src/server.rs
+++
@@ -1,5 +0,0 @@
-use tonic::{transport::Server, Request, Response, Status};
-
-pub mod hello_world {
-    tonic::include_proto!("urlgen"); // The string specified here must match the proto package name
-}
